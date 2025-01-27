0cdb32a761d57802557396754d20402ab036ddab
Ludovic <ludovic.germain@itron.com>
2025-01-27 15:04:47 +0100

    Remove password from DB URL

--- src/service/run_ludhome_mqtt_saver.sh
+++ src/service/run_ludhome_mqtt_saver.sh
@@ -4,7 +4,7 @@ echo "Starting LudHome MQTT Saver"
 
 cd /mnt/data/ludhome
 
-export DATABASE_URL='postgres://ludhome:sdiaea@localhost/ludhome'
+export DATABASE_URL='postgres://ludhome@localhost/ludhome'
 export RUST_LOG='info'
 
 target/release/ludhome_mqtt_saver
